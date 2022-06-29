5f6fbc09f2ab4fd3b0884f6112a8730563c994e8
Arjun1932010 <83018389+Arjun1932010@users.noreply.github.com>
2022-06-29 19:11:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,14 +8,19 @@ function setup(){
 
 function draw(){
     image(video,230,150,220,200);
+    fill("red");
     circle(50,50,80);
-    fill("red");
-    circle(550,50,80);
-    fill("red");
-    circle(550,430,80);
-    fill("red");
-    circle(50,430,80)
     
+    circle(550,50,80);
+    
+    circle(550,430,80);
+ 
+    circle(50,430,80);
+    fill("blue");
+    rect(540,90,20,300);
+    rect(90,430,420,20);
+    rect(40,90,20,300);
+    rect(90,40,420,20);
 }
 
 function take_snapshot(){
